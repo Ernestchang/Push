91bd09df1206c798a57b6628c71dece89806666f
ernestchang <ernestchang@126.com>
2018-09-18 08:33:01 +0800

    fix

--- push/src/main/java/com/ernest/push/target/huawei/HuaweiPushRevicer.java
+++ push/src/main/java/com/ernest/push/target/huawei/HuaweiPushRevicer.java
@@ -21,10 +21,10 @@ import java.util.List;
  * onToken 调用getToken方法后，获取服务端返回的token结果，返回token以及belongId
  * onPushState 调用getPushState方法后，获取push连接状态的查询结果
  * onPushMsg 推送消息下来时会自动回调onPushMsg方法实现应用透传消息处理。本接口必须被实现。 在开发者网站上发送push消息分为通知和透传消息
- *           通知为直接在通知栏收到通知，通过点击可以打开网页，应用 或者富媒体，不会收到onPushMsg消息
- *           透传消息不会展示在通知栏，应用会收到onPushMsg
+ * 通知为直接在通知栏收到通知，通过点击可以打开网页，应用 或者富媒体，不会收到onPushMsg消息
+ * 透传消息不会展示在通知栏，应用会收到onPushMsg
  * onEvent 该方法会在设置标签、点击打开通知栏消息、点击通知栏上的按钮之后被调用。由业务决定是否调用该函数。
- *
+ * <p>
  * Application needs to create a subclass to inherit com.huawei.hms.support.api.push.PushReceiver,
  * implement Ontoken,onpushstate, Onpushmsg,onevent,
  * these several abstract methods, Used to receive token return, push connection status,
@@ -43,9 +43,9 @@ import java.util.List;
  */
 public class HuaweiPushRevicer extends PushReceiver {
 
-	public static final String TAG = "HuaweiPushRevicer";
+    public static final String TAG = "HuaweiPushRevicer";
 
-	public static final String ACTION_UPDATEUI = "action.updateUI";
+    public static final String ACTION_UPDATEUI = "action.updateUI";
     public static final String ACTION_TOKEN = "action.updateToken";
 
     private static List<IPushCallback> pushCallbacks = new ArrayList<IPushCallback>();
@@ -69,32 +69,34 @@ public class HuaweiPushRevicer extends PushReceiver {
 
     @Override
     public void onToken(Context context, String tokenIn, Bundle extras) {
-    	String belongId = extras.getString("belongId");
+        String belongId = extras.getString("belongId");
 
         Intent intent = new Intent();
         intent.setAction(ACTION_TOKEN);
         intent.putExtra(ACTION_TOKEN, tokenIn);
         callBack(intent);
 
-        intent = new Intent();
-        intent.setAction(ACTION_UPDATEUI);
-        intent.putExtra("log", "belongId is:" + belongId + " Token is:" + tokenIn);
-        callBack(intent);
+//        intent = new Intent();
+//        intent.setAction(ACTION_UPDATEUI);
+//        intent.putExtra("log", "belongId is:" + belongId + " Token is:" + tokenIn);
+//        callBack(intent);
     }
 
     @Override
     public boolean onPushMsg(Context context, byte[] msg, Bundle bundle) {
         try {
-        	//CP可以自己解析消息内容，然后做相应的处理 | CP can parse message content on its own, and then do the appropriate processing
+            //CP可以自己解析消息内容，然后做相应的处理 | CP can parse message content on its own, and then do the appropriate processing
             String content = new String(msg, "UTF-8");
             Intent intent = new Intent();
             intent.setAction(ACTION_UPDATEUI);
-            intent.putExtra("log", "Receive a push pass message with the message:" + content);
+//            intent.putExtra("log", "Receive a push pass message with the message:" + content);
+            intent.putExtra("log", content);
             callBack(intent);
         } catch (Exception e) {
             Intent intent = new Intent();
             intent.setAction(ACTION_UPDATEUI);
-            intent.putExtra("log", "Receive push pass message, exception:" + e.getMessage());
+//            intent.putExtra("log", "Receive push pass message, exception:" + e.getMessage());
+            intent.putExtra("log", e.getMessage());
             callBack(intent);
         }
         return false;
@@ -115,6 +117,7 @@ public class HuaweiPushRevicer extends PushReceiver {
         }
 
         String message = extras.getString(BOUND_KEY.pushMsgKey);
+//        intent.putExtra("log", "Received event,notifyId:" + notifyId + " msg:" + message);
         intent.putExtra("log", "Received event,notifyId:" + notifyId + " msg:" + message);
         callBack(intent);
         super.onEvent(context, event, extras);
@@ -124,7 +127,8 @@ public class HuaweiPushRevicer extends PushReceiver {
     public void onPushState(Context context, boolean pushState) {
         Intent intent = new Intent();
         intent.setAction(ACTION_UPDATEUI);
-        intent.putExtra("log", "The Push connection status is:" + pushState);
+//        intent.putExtra("log", "The Push connection status is:" + pushState);
+        intent.putExtra("log", pushState);
         callBack(intent);
     }
 
